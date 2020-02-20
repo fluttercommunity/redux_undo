eca511817d1eb7ea109b7f013c64cd8f3bac4ee1
Michel Engelen <michel@nomorejs.net>
2020-02-20 14:57:41 +0100

    changed travis CI config

--- example/lib/redux/simple_counter/simple_counter_actions.dart
+++ example/lib/redux/simple_counter/simple_counter_actions.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/widgets.dart';
 import 'package:redux_undo/redux_undo.dart';
 
-/// Counter actions
-
+/// [CounterIncrement] action class
+/// increments the counter at the given index
 class CounterIncrement {
   CounterIncrement({
     @required this.index,
   });
 
+  // index of the counter that should be incremented
   final int index;
 }
 
+/// [CounterDecrement] action class
+/// decrements the counter at the given index
 class CounterDecrement {
   CounterDecrement({
     @required this.index,
   });
 
+  // index of the counter that should be decremented
   final int index;
 }
 
