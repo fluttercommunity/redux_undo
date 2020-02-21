1fec915e6dd7b45a46dfe470e4982211dfe7cc5d
Michel Engelen <michel@nomorejs.net>
2020-02-21 08:08:42 +0100

    Revert "changed travis CI config"
    This reverts commit eca51181

--- example/lib/redux/simple_counter/simple_counter_actions.dart
+++ example/lib/redux/simple_counter/simple_counter_actions.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/widgets.dart';
 import 'package:redux_undo/redux_undo.dart';
 
-/// [CounterIncrement] action class
-/// increments the counter at the given index
+/// Counter actions
+
 class CounterIncrement {
   CounterIncrement({
     @required this.index,
   });
 
-  // index of the counter that should be incremented
   final int index;
 }
 
-/// [CounterDecrement] action class
-/// decrements the counter at the given index
 class CounterDecrement {
   CounterDecrement({
     @required this.index,
   });
 
-  // index of the counter that should be decremented
   final int index;
 }
 
