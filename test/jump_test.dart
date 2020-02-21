5bc452856c20e4ac27a6c0f88a9ba1c79cd06e69
Michel Engelen <michel@nomorejs.net>
2020-02-21 12:39:53 +0100

    added JumpAction tests

---
+++ test/jump_test.dart
@@ -0,0 +1,56 @@
+import 'package:redux/redux.dart';
+import 'package:redux_undo/redux_undo.dart';
+import 'package:test/test.dart';
+
+import 'test_data.dart';
+
+// We create the State reducer by combining many smaller reducers into one!
+TestState rootReducer(TestState state, dynamic action) {
+  return TestState(
+    counter: intReducer(state.counter, action),
+    title: stringReducer(state.title, action),
+  );
+}
+
+final Store<UndoableState<TestState>> store = Store<UndoableState<TestState>>(
+  createUndoableReducer<TestState>(rootReducer),
+  initialState: createUndoableState(TestState.initial(), false),
+);
+
+final String updatedTitle = 'Updated Title';
+final int jumpAmount = 4;
+
+void main() {
+  group('State preparation: ', () {
+    test('update title String', () {
+      store.dispatch(StringTestAction(value: updatedTitle));
+      expect(store.state.present.title, equals(updatedTitle));
+      expect(store.state.present.counter, equals(INITIAL_COUNTER));
+    });
+
+    test('update counter', () {
+      for (var i = 0; i < jumpAmount; i++) {
+        store.dispatch(IncrementTestAction());
+      }
+
+      expect(store.state.present.counter, equals(jumpAmount));
+      expect(store.state.past.length, equals(jumpAmount + 1));
+    });
+  });
+
+  group('JumpAction', () {
+    test('is performed correctly', () {
+      store.dispatch(UndoableJumpAction(index: -jumpAmount));
+      expect(store.state.present.counter, equals(0));
+      expect(store.state.present.title, equals(updatedTitle));
+    });
+
+    test('past has length of 1', () {
+      expect(store.state.past.length, equals(1));
+    });
+
+    test('future has length of 4', () {
+      expect(store.state.future.length, equals(jumpAmount));
+    });
+  });
+}
--- test/redux_undo_test.dart
+++ test/redux_undo_test.dart
@@ -1,13 +1,15 @@
 import 'blacklist_test.dart' as blacklist_test;
+import 'jump_test.dart' as jump_test;
+import 'limit_test.dart' as limit_test;
 import 'state_test.dart' as state_test;
 import 'undo-redo_test.dart' as undo_redo_test;
 import 'whitelist_test.dart' as whitelist_test;
-import 'limit_test.dart' as limit_test;
 
 void main() {
+  blacklist_test.main();
+  jump_test.main();
+  limit_test.main();
   state_test.main();
   undo_redo_test.main();
-  limit_test.main();
   whitelist_test.main();
-  blacklist_test.main();
 }
